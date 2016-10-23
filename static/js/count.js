ede8281464c0754ed852a30caa6a5a3e8ceb6b28
Javier Martín Alonso <xavimartin@hotmail.com>
2016-10-23 02:41:39 +0200

    comentarios

--- static/js/count.js
+++ static/js/count.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
+    /*    
     pre_content = $("#javiermartinalonso_site_pv").html();
     $("#javiermartinalonso_site_pv").html(pre_content);
 
-    /*
+
     * TODO: Tiene montado un servicio rest propio que le devuelve el numero de visitas
     * habria que cambiar esto o siempre nos dara el numero de visitas de otro
     * url: "http://jalpc-a.leanapp.cn/api/jalpc_count",
