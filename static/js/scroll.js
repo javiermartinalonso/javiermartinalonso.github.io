215ebbca93fc146a795a16fffae2d31ba47820a0
Javier Martín Alonso <xavimartin@hotmail.com>
2016-11-04 13:02:56 +0100

    boton home logo

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -20,7 +20,7 @@ var cbpAnimatedHeader = (function() {
 
     function scrollPage() {
       $('.navbar-default').toggleClass('navbar-scroll', scrollY() >= changeHeaderOn);
-      $('.btn-logo-home-default').removeClass('btn-logo-home-default').addClass('btn-logo-home-scrol');
+      $('.btn-logo-home-default').removeClass('btn-logo-home-default').addClass('btn-logo-home-scroll');
     }
 
     function scrollY() {
