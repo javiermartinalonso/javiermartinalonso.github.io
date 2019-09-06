4be7d54c53ab1dc23219124afb0c696f89fbd829
Javier Martín Alonso <xavimartin@hotmail.com>
2019-09-06 19:50:16 +0200

    borradores BPM

---
+++ drafts/SATEC/CONCURSO-IDEAS-SATEC/IDEAS_IMPLEMENTACION/¿Cómo usar Application Metrics (javametrics) en una aplicación Spring Boot_ – Un poco de Java y +_files/rate.php
@@ -0,0 +1,6 @@
+PDRTJS_2157597_post_20399.avg_rating = 0;
+PDRTJS_2157597_post_20399.votes = 0;		
+PDRTJS_settings_2157597_post_20399= {			"type" : "stars",			"size" : "sml",			"star_color" : "yellow",			"custom_star" : "",			"font_size" : "",			"font_line_height" : "16px",			"font_color" : "",			"font_align" : "left",			"font_position" : "right",			"font_family" : "",			"font_bold" : "normal",			"font_italic" : "normal",			"text_votes" : "Votes",			"text_rate_this" : "Rate This",			"text_1_star" : "Very Poor",			"text_2_star" : "Poor",			"text_3_star" : "Average",			"text_4_star" : "Good",			"text_5_star" : "Excellent",			"text_thank_you" : "Thank You",			"text_rate_up" : "Rate Up",			"text_rate_down" : "Rate Down"		};
+PDRTJS_2157597_post_20399.init();		
+PDRTJS_2157597_post_20399.token='672b072bc00b662e9ee698253b4ed0ea';
+/*2157597,_post_20399,wp-post-20399,3584303445,0-0*/
