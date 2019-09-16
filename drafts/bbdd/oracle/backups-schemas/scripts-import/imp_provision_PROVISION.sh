4936fc9838435336a4f72ef02b8196cafa89895f
javimartinalonso <javimartinalonso@gmail.com>
2019-09-16 19:34:55 +0200

    oracle

--- drafts/bbdd/oracle/backups-schemas/scripts-import/imp_provision_PROVISION.sh
+++ drafts/bbdd/oracle/backups-schemas/scripts-import/imp_provision_PROVISION.sh
@@ -2,4 +2,4 @@ ORACLE_SID="bpm_pro1"
 dia=$(date +"%Y-%m-%d_%H_%M")
 echo $ORACLE_SID
 
-impdp system/miguel33 DIRECTORY=dp DUMPFILE=expdp_bpm_prov1_provision_2019-01-15_09_44.dmp LOGFILE=impdp_${ORACLE_SID}_${dia}.log SCHEMAS=PROVISION TABLE_EXISTS_ACTION=replace
+impdp system/miguel33 DIRECTORY=dp DUMPFILE=expdp_bpm_prov1_provision_2019-01-15_09_44.dmp LOGFILE=impdp_${ORACLE_SID}_${dia}.log SCHEMAS=PROVISION REMAP_SCHEMA=PROVISION_DES:PROVISION TABLE_EXISTS_ACTION=replace
